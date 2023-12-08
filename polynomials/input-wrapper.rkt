c88b7e9c4314df08935de7b7d346c1efa0708c32
Henrique <henriqueladeira@Henriques-MacBook-Pro.local>
2023-12-07 23:47:14 -0300

    refactor

--- polynomials/input-wrapper.rkt
+++ polynomials/input-wrapper.rkt
@@ -17,11 +17,15 @@
 ; Wrapper to function with 2 inputs
 (define (double-input-wrapper fcn arg1 arg2)
   (if (and (is-dense? arg1) (is-dense? arg2))
+
     (fcn arg1 arg2)
+
     (if (is-dense? arg1)
       (fcn arg1 (to-dense arg2))
+
       (if (is-dense? arg2)
         (fcn (to-dense arg1) arg2)
+        
         (to-sparse (fcn (to-dense arg1) (to-dense arg2)))
       )
     )
